ed13402baae79b600479c63ae8eca58e7465a9f2
John Forster <contact@johnforster.dev>
2025-07-13 18:05:42 +0100

    Replace svg icon and add others

--- vite.config.ts
+++ vite.config.ts
@@ -24,14 +24,20 @@ export default defineConfig({
         theme_color: "#30263a",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "icon-512.png",
             sizes: "512x512",
             type: "image/png",
+          },
+          {
+            src: "icon-mask.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
